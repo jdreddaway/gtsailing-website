dd53ee4f746a11de36872fe571b856c13c8cdb98
jdreddaway@gmail.com <jdreddaway@gmail.com@410c1bdc-8c4c-b6be-ae90-20a510e236ad>
2014-02-19 20:37:16 +0000

    Committing current work to back up what I currently have.

--- root/index.php
+++ root/index.php
@@ -3,37 +3,47 @@
 <html>
 <head>
 	<title>Sailing Club at Georgia Tech</title>
+	<link rel="stylesheet" type="text/css" href="/common.css" />
 	<link rel="stylesheet" type="text/css" href="/index.css" />
 </head>
 <body>
-	<div id="fb-root"></div>
-	<script type="text/javascript">
-		(function(d, s, id) {
-		  var js, fjs = d.getElementsByTagName(s)[0];
-		  if (d.getElementById(id)) return;
-		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=664212880267915";
-		  fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-	</script>
-
-	<div id="content">
-		<div id="top">
-			<img id="logo" src="/images/gt-sails logo.png" alt="GT Sailing Logo" />
-			<p id="title">
-				Sailing Club at Georgia Tech
+	<div>
+		<div class="bgwrapper">
+			<img alt="GT Sailing Burgee" src="/images/backgrounds/burgee-bordered.png" />
+		</div>
+	</div>
+	<div>
+		<div class="bgwrapper">
+			<img alt="420s downwind" src="/images/backgrounds/photo1.jpg" />
+		</div>
+		<div id="what_we_do" class="description">
+			<h2  class="description">What do we do?</h2>
+		</div>
+		<div id="we_sail" class="description">
+			<h2 class="description">#Sail.</h2>
+		</div>
+	</div>
+	<div>
+		<div class="bgwrapper">
+			<img alt="llsc beach" src="/images/backgrounds/beach.jpg" />
+		</div>
+		<div id="whenever" class="description">
+			<h2>Whenever We Want</h2>
+			<p>
+				GT Sailing owns a large number of boats at Lake Lanier Sailing Club.
+				LLSC student members called "lakehosts" can take members of GTSC sailing any time, any day, any month. 
 			</p>
 		</div>
-		<ul id="nav">
-			<li><a href="/news.php">News</a></li>
-			<li><a href="/club_info.php">Club Info</a></li>
-			<li><a href="/fleet.php">Fleet</a></li>
-			<li><a href="/calendar.php">Calendar</a></li>
-			<li><a href="/instruction.php">Instruction</a></li>
-			<li><a href="/competition.php">Competition</a></li>
-			<li><a href="/contact/">Contact Us</a></li>
-		</ul>
-		<div class="fb-like" style="display: block;" data-href="https://www.facebook.com/gtsailingclub" data-width="300px" data-layout="standard" data-action="like" data-show-faces="true" data-share="false">
+	</div>
+	<div>
+		<div class="bgwrapper">
+			<img alt="collegiate race team" src="/images/backgrounds/saisa open fall 2013.jpg" />
+		</div>
+		<div id="raceteam" class="description dark-font">
+			<h2>Collegiate Race Team</h2>
+			<p>
+				The GT Sailing Race Team competes against other schools nearly every weekend during the Fall and Spring semesters.
+			</p>
 		</div>
 	</div>
 </body>
