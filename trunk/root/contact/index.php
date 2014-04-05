6fceb46f4cece2b4a6bfd5140daa7ee9cd3584bc
jdreddaway@gmail.com <jdreddaway@gmail.com@410c1bdc-8c4c-b6be-ae90-20a510e236ad>
2014-04-05 04:35:47 +0000

    Reorganized everything.

--- trunk/root/contact/index.php
+++ trunk/root/contact/index.php
@@ -1,8 +1,8 @@
 <?php
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/scripts/common.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/common/server-scripts.php");
 
 	static $pageName = "contact";
 	static $title = "Contact Us";
 	
-	requireOnce("/scripts/template.php");
+	requireOnce("/common/old.template.php");
 ?>
---
+++ trunk/root/home/home.content.php
@@ -0,0 +1,37 @@
+<ul id="scrollnav">
+	<li><a onclick="scrollTo('what_we_do');"><img src="images/little circle gold.png" /></a></li>
+	<li><a onclick="scrollTo('whenever');"><img src="images/little circle gold.png" /></a></li>
+	<li><a onclick="scrollTo('raceteam');"><img src="images/little circle gold.png" /></a></li>
+</ul>
+<div class="dynamic">
+	<h2>Sailing Club at Georgia Tech</h2>
+	<img class="medimg" alt="GT Sailing Burgee" src="/images/backgrounds/burgee.png" />
+</div>
+<div class="fixed">
+	<img alt="420s downwind" class="background" src="/images/backgrounds/glass at eckerd.jpg" />
+	<div id="what_we_do" class="dark description">
+		<h2  class="description">What do we do?</h2>
+	</div>
+	<div id="we_sail" class="dark description">
+		<h2 class="description">#Sail.</h2>
+	</div>
+</div>
+<div class="fixed">
+	<img alt="llsc beach" class="background" src="/images/backgrounds/beach.jpg" />
+	<div id="whenever" class="dark description">
+		<h2>Whenever We Want</h2>
+		<p>
+			GT Sailing owns a large number of boats at Lake Lanier Sailing Club.
+			LLSC student members called "lakehosts" can take members of GTSC sailing any time, any day, any month. 
+		</p>
+	</div>
+</div>
+<div class="fixed">
+	<img alt="collegiate race team" class="background" src="/images/backgrounds/saisa open fall 2013.jpg" />
+	<div id="raceteam" class="light description">
+		<h2>Collegiate Race Team</h2>
+		<p>
+			The GT Sailing Race Team competes against other schools nearly every weekend during the Fall and Spring semesters.
+		</p>
+	</div>
+</div>
