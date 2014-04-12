cd3e1db4e25e940b2fa5699becf560afe02fb02c
jdreddaway@gmail.com <jdreddaway@gmail.com@410c1bdc-8c4c-b6be-ae90-20a510e236ad>
2014-04-12 21:33:16 +0000

    Added smooth scrolling to scrollnav.

--- trunk/root/common/flow.template.php
+++ trunk/root/common/flow.template.php
@@ -14,6 +14,8 @@
 	<link rel="stylesheet" href="/common/flow.css" />
 	<link rel="stylesheet" href="<?php print("/$pageName/$pageName.css");?>" />
 	<script type="text/javascript" src="/common/client-scripts.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script type="text/javascript" src="/common/jquery.scrollintoview.min.js"></script>
 </head>
 <body>
 	<?php requireOnce("/$pageName/$pageName.content.php"); ?>
