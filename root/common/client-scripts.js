d36b000795f646d0ce87f4230fbbc8e18e43a6b0
jdreddaway@gmail.com <jdreddaway@gmail.com@410c1bdc-8c4c-b6be-ae90-20a510e236ad>
2014-04-12 03:49:46 +0000

    Scrollnav now indicates which section you are currently on. Scrollnav no longer uses images. It uses CSS instead. Fixed firefox weirdness that selected the background image after clicking only once.

--- root/common/client-scripts.js
+++ root/common/client-scripts.js
@@ -1,3 +1,5 @@
+var currentlyActiveScrollLink = null;
+
 function scrollTo(elementID) {
 	document.getElementById(elementID).scrollIntoView();
 }
@@ -13,12 +15,10 @@ function setUpScrollNav() {
 		section = children[i];
 	
 		if (section.tagName == "DIV" && section.id != "") {
-			var li = document.createElement("li");
-			var link = document.createElement("a");
-			link.setAttribute("onclick", "scrollTo('" + section.id + "');");
+			var link = createScrollNavLink(section.id);
+			scrollNav.appendChild(link);
 			
-			li.appendChild(link);
-			scrollNav.appendChild(li);
+			section.addEventListener("mouseover", function() { setActiveScrollLink(this.id); });
 			
 			numSections++;
 		}
@@ -27,4 +27,29 @@ function setUpScrollNav() {
 	if (numSections > 1) {
 		document.body.appendChild(scrollNav);
 	}
+}
+
+function createScrollNavLink(targetID) {
+	var li = document.createElement("li");
+	var link = document.createElement("a");
+	link.addEventListener("click", function() { scrollTo(targetID); setActiveScrollLink(targetID); });
+	link.id = getLinkID(targetID);
+	
+	li.appendChild(link);
+	
+	return li;
+}
+
+function setActiveScrollLink(sectionID) {
+	if (currentlyActiveScrollLink !== null) {
+		currentlyActiveScrollLink.className = "";
+	}
+	
+	var linkID = getLinkID(sectionID);
+	currentlyActiveScrollLink = document.getElementById(linkID);
+	currentlyActiveScrollLink.className = "active";
+}
+
+function getLinkID(sectionID) {
+	return sectionID + "_link";
 }
