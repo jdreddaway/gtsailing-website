b17e948cce00bc7efc7e6c578ca118786581904c
jdreddaway@gmail.com <jdreddaway@gmail.com@410c1bdc-8c4c-b6be-ae90-20a510e236ad>
2014-05-14 03:11:32 +0000

    Extracted footer content into includable php file for reuse. Added test article to help design layout. Remade calendar. Added honeycomb background pattern.

---
+++ root/articles/test/test.content.php
@@ -0,0 +1,15 @@
+<h1>Test Article</h1>
+<p>
+	School is out for summer! Yippee!
+	The weathered sailors of the GT Raceteam all go their separate ways.
+	Some to Pennsylvania or Jersey to visit family; some to Chicago or Boston for internships; and still others to the Carribean to simply enjoy themselves.
+	But there is still one thing that they all have in common: each is thinking about sailing. 
+</p>
+<p>
+	The team finished 8th at the SAISA Spring Championship&mdash;only 2 points out of 6th, which would have qualified them for semi-finals at St. Mary's College.
+	Slightly disappointed, they were warmed by the knowledge that GT Sailing is going to kick ass next year.
+	Since the team is fairly young, it will not lose any serious raceteam members (although a sincere shoutout to Danny Rodriguez, Matt Salley, and Abhay Baliga is well deserved).
+	In addition, hopes are high for the incoming team in the fall.
+	Not only will super-sailor Martin Kendrick be returing from his semester off, but there are also rumors of talented high school sailors coming to Tech.
+	Only time will tell, but be warned: someone needs to keep an eye on that GT Sailing Raceteam.
+</p>
