8c4237d68f0cd6ba6a9861ae9866674fda1a3786
jdreddaway@gmail.com <jdreddaway@gmail.com@410c1bdc-8c4c-b6be-ae90-20a510e236ad>
2014-05-14 04:08:36 +0000

    Added construction.html to use when we don't want users going to a page yet. Only use when releasing website or when page does not exist yet. Updated many pages whose background image alts were incorrect. Cropped background images to 1.6:1 ratio (ratio of many computer screens). Added some content to raceteam.

--- root/raceteam/raceteam.content.php
+++ root/raceteam/raceteam.content.php
@@ -1,17 +1,20 @@
 <div id="activity" class="fixed">
-	<img alt="420s downwind" class="background" src="/images/backgrounds/saisa open fall 2013 upwind.jpg" />
+	<img alt="SAISA Open Fall 2013 upwind" class="background" src="/images/backgrounds/saisa open fall 2013 upwind.jpg" />
 	<div id="activity_d" class="dark">
 		<h2>Many Regattas Every Semester</h2>
+		<p>
+			During the 2013-2014 year, we went to 9 regattas in the Fall and 8 in the Spring&mdash;and we plan to attend even more next year!
+		</p>
 	</div>
 </div>
 <div id="practices" class="fixed">
-	<img alt="420s downwind" class="background" src="/images/backgrounds/glass at eckerd.jpg" />
+	<img alt="racing upwind at LLSC" class="background" src="/images/backgrounds/racing upwind llsc.jpg" />
 	<div id="practices_d" class="dark">
 		<h2>Weekly Practices</h2>
 	</div>
 </div>
 <div id="funding" class="dynamic">
-	<img id="sga" alt="420s downwind" src="/images/backgrounds/SGA-3.jpg" />
+	<img id="sga" alt="sga" src="/images/backgrounds/SGA-3.jpg" />
 	<div id="funding_d" class="dark">
 		<h2>School Funding</h2>
 		<p>
@@ -19,6 +22,15 @@
 			This means that the sailors who go to the event have to split gas, vehicle rental, and hotel expenses.
 			Fortunately, the CRC has SUVs and 12 passenger vans that we can rent for only $35 per day.
 			We also have a lot of amazing alumni and friends who let us stay at their houses for free.
+		</p>
+	</div>
+</div>
+<div id="collegesailing" class="fixed">
+	<img alt="glass at eckerd" class="background" src="/images/backgrounds/glass%20at%20eckerd.jpg" />
+	<div id="icsa" class="dark">
+		<h2>Inter-Collegiate Sailing Association</h2>
+		<p>
+			TODO add links to college sailing resources
 		</p>
 	</div>
 </div>
