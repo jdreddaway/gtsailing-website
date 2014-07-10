7d969a5716539db7a57136a121fa2c6a6424a428
jdreddaway@gmail.com <jdreddaway@gmail.com@410c1bdc-8c4c-b6be-ae90-20a510e236ad>
2014-07-10 04:03:19 +0000

    Touched up raceteam page.

--- root/raceteam/raceteam.content.php
+++ root/raceteam/raceteam.content.php
@@ -1,6 +1,29 @@
+<div id="club" class="fixed">
+	<img alt="Wilmington Upwind" class="background" src="/images/backgrounds/IMG_2025.jpg" />
+	<div id="club_d" class="light">
+		<h2>Club Team</h2>
+		<div>
+			<p>
+				We are a completely student-run club team&mdash;
+				we plan our practices, trips, and regattas ourselves.
+				We do not have a coach.
+				We do not get free housing or meal plans or scholarships for sailing.
+			</p>
+			<p>
+				<strong>But that does not hold us back. We are one helluva team.</strong>
+			</p>
+			<p>
+				We are still fairly competitive against other Varsity teams, and we know how to have a lot more fun than them.
+				All we have is each other, and that makes us a pretty close-knit group.
+				Personally, my best friends are the ones I sail with every weekend.
+			</p>
+		</div>
+	</div>
+</div>
+
 <div id="activity" class="fixed">
 	<img alt="SAISA Open Fall 2013 upwind" class="background" src="/images/backgrounds/saisa open fall 2013 upwind.jpg" />
-	<div id="activity_d" class="dark">
+	<div id="activity_d" class="light">
 		<h2>Many Regattas Every Semester</h2>
 		<p>
 			During the 2013-2014 year, we went to 9 regattas in the Fall and 8 in the Spring&mdash;and we plan to attend even more next year!
@@ -16,7 +39,7 @@
 <div id="funding" class="dynamic">
 	<div id="funding_bg">
 		<img id="sga" alt="sga" src="/images/backgrounds/SGA-3.jpg" />
-		<div id="funding_d" class="dark">
+		<div id="funding_d" class="light">
 			<h2>School Funding</h2>
 			<p>
 				As far as traveling to regattas goes, Georgia Tech only funds 2/3 of the gas for 4 regattas every year.
@@ -27,12 +50,16 @@
 		</div>
 	</div>
 </div>
+
+<!-- TODO add this when collegesailing.org starts working again
 <div id="collegesailing" class="fixed">
 	<img alt="glass at eckerd" class="background" src="/images/backgrounds/glass%20at%20eckerd.jpg" />
 	<div id="icsa" class="dark">
 		<h2>Inter-Collegiate Sailing Association</h2>
 		<p>
-			TODO add links to college sailing resources
+			Link to scores
+			Link to sign up page
 		</p>
 	</div>
 </div>
+-->
