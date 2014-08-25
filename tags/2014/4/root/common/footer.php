9b8e45750915a88bde99dd1f9381023515162a73
jdreddaway@gmail.com <jdreddaway@gmail.com@410c1bdc-8c4c-b6be-ae90-20a510e236ad>
2014-08-25 21:21:55 +0000

    Creating tag.

---
+++ tags/2014/4/root/common/footer.php
@@ -0,0 +1,8 @@
+<table>
+	<tr>
+		<td><a href="/">Home</a></td>
+		<td><a href="/contact/">Contact</a></td>
+		<td><a href="/calendar/">Calendar</a></td>
+		<td><a href="/rules/">Constitution and Bylaws</a></td>
+	</tr>
+</table>
---
+++ tags/2014/4/root/home/home.content.php
@@ -0,0 +1,89 @@
+<div id="intro" class="fixed">
+	<img alt="sunset at llsc" class="background" src="/images/backgrounds/sunset-cropped.jpg" />
+	<div id="intro_content">
+		<div id="top">
+			<img id="logo" src="/images/gt-sails logo.png" alt="GT Sailing Logo" />
+			<h2 id="title">
+				Sailing Club at Georgia Tech
+			</h2>
+		</div>
+		<ul id="nav">
+			<!--<li><a href="/news.php">News</a></li>-->
+			<li><a href="/calendar/">Calendar</a></li>
+			<li><a href="/contact/">Contact Us</a></li>
+			<li><a href="/rules/">Rules</a></li>
+		</ul>
+		<div id="fb-root"></div>
+		<script type="text/javascript">
+			(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=664212880267915";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+		</script>
+		<div class="fb-like" style="display: block;" data-href="https://www.facebook.com/gtsailingclub" data-width="300px" data-layout="standard" data-action="like" data-show-faces="true" data-share="false">
+		</div>
+	</div>
+</div>
+<div id="what" class="fixed">
+	<img alt="420s downwind" class="background" src="/images/backgrounds/glass at eckerd.jpg" />
+	<div id="what_we_do" class="dark">
+		<h2>What do we do?</h2>
+	</div>
+	<div id="we_sail" class="dark">
+		<h2>#Sail.</h2>
+	</div>
+</div>
+<div id="sailing" class="fixed">
+	<img alt="llsc beach" class="background" src="/images/backgrounds/beach.jpg" />
+	<div id="whenever" class="dark">
+		<h2>Whenever We Want</h2>
+		<p>
+			GT Sailing owns a large number of boats at Lake Lanier Sailing Club.
+			LLSC student members called <a href="/rules/#lakehosts">"lakehosts"</a> can take members of GTSC sailing any time, any day, any month. 
+		</p>
+	</div>
+</div>
+<div id="fleet" class="fixed">
+	<img alt="" class="background" src="/images/backgrounds/DSC09563 fall break 2012.jpg" />
+	<div id="fleet_d" class="dark">
+		<h2><a href="/fleet/">Lots of Boats</a></h2>
+		<p>
+			Over the years we have acquired a variety of different boats&mdash;from dinghies to keelboats to windsurfers.
+			You should check out our <a href="/fleet/">fleet</a>.
+		</p>
+	</div>
+</div>
+<div id="racing" class="fixed">
+	<img alt="collegiate race team" class="background" src="/images/backgrounds/saisa open fall 2013.jpg" />
+	<div id="raceteam" class="light">
+		<h2><a href="/raceteam/">Collegiate Race Team</a></h2>
+		<p>
+			The GT Sailing <a href="/raceteam/">Race Team</a> competes against other schools nearly every weekend during the Fall and Spring semesters.
+		</p>
+	</div>
+</div>
+<div id="organization" class="fixed">
+	<img alt="" class="background" src="/images/backgrounds/DSCF1487.jpg" />
+	<div id="org_d" class="dark">
+		<h2>Organization</h2>
+		<div>
+			<p>
+				We are a legitimate tier 3 sports club at Georgia Tech, which means that, upon other things, we have an <a href="http://sga.gatech.edu/" target="_blank">SGA</a> budget.
+				We have an awesome <a href="/structure">exec board</a>, which manages the club and plans our events.
+			</p>
+			<p>
+				Currently, we meet in CRC 251 at 7:30 on Mondays.
+				Come stop by to learn more!
+				If you can't make a meeting, feel free to <a href="/contact">contact</a> us!
+			</p>
+		</div>
+	</div>
+</div>
+<!-- TODO finish
+<div id="alumni" class="fixed">
+
+</div>
+-->
