40b3d7320169b91bb6864dcc6f7d37f48cdfbf0f
jdreddaway@gmail.com <jdreddaway@gmail.com@410c1bdc-8c4c-b6be-ae90-20a510e236ad>
2014-08-25 21:56:45 +0000

    Putting articles/test under construction.

--- trunk/root/articles/test/index.php
+++ trunk/root/articles/test/index.php
@@ -1,6 +1,8 @@
 <?php
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/common/server-scripts.php");
 	
+	underConstruction();
+	
 	static $pageName = "test";
 	static $path = "articles/test";
 	static $title = "Test";
